0eb2c0e4bd60cd7c7cea133f36415a8cc0249ef1
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-12-22 16:09:16 +0530

    Practical-2 Add start and terminated msg for logcat

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Start","Activity Created");
     }
 
-    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Start","Activity Start");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("End","Activity Terminated");
+
+    }
 }
